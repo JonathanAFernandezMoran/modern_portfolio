eabb198335c14ce551908c32162e973439f80798
JonathanAFernandezMoran <f.jonathan123@gmail.com>
2022-03-02 16:04:32 -0600

    Index markup & main sass

---
+++ dist/js/main.js
@@ -0,0 +1,19 @@
+//Select Document Object Modules Items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelector('.nav-item');
+
+//Set Initial State oof Menu
+let showMenu = false;
+
+menuBtn.addEventListener('click',toggleMenu);
+
+function toggleMenu() {
+    if(!showMenu) {
+
+    } else {
+
+    }
+}
